96c6b8cb31d360a580b7c8a29b4dde5e7e4e90cc
Omar Cantu <OmarCantu@users.noreply.github.com>
2019-04-21 23:00:12 +1000

    Add blur functionality to text input on enter

--- src/components/textInput/TextInput.js
+++ src/components/textInput/TextInput.js
@@ -1,49 +1,113 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 import classNames from 'classnames';
 
 import styles from './TextInput.scss';
 
-const TextInput = props => {
-	const { className, label, name, onChange, placeholder } = props;
+class TextInput extends Component {
+  
+  static propTypes = {
+    className: PropTypes.string,
+    label: PropTypes.string,
+    name: PropTypes.string,
+    onChange: PropTypes.func,
+    placeholder: PropTypes.string
+  };
 
-	const componentClassName = classNames(styles.component, className);
+  static defaultProps = {
+    className: undefined,
+    label: undefined,
+    name: undefined,
+    onChange: undefined,
+    placeholder: undefined
+  };
 
-	return (
-    <div className={componentClassName}>
-      {label && (
-        <label className={styles.label} htmlFor={name}>
-          {label}
-        </label>
-      )}
+  constructor(props) {
+    super(props);
+    this.textInput = React.createRef();
+  }
 
-      <input
-        className={styles.input}
-        id={name}
-        maxLength="30"
-        name={name}
-        onChange={onChange}
-        placeholder={placeholder}
-        type="text"
-      />
-    </div>
-  );
-};
+  blurTextInput = () => {
+    this.textInput.current.blur();
+  }
 
-TextInput.propTypes = {
-	className: PropTypes.string,
-	label: PropTypes.string,
-	name: PropTypes.string,
-	onChange: PropTypes.func,
-	placeholder: PropTypes.string
-};
+  render () {
+    const { className, label, name, onChange, placeholder } = this.props;
 
-TextInput.defaultProps = {
-	className: undefined,
-	label: undefined,
-	name: undefined,
-	onChange: undefined,
-	placeholder: undefined
+    const componentClassName = classNames(styles.component, className);
+
+    return (
+      <div className={componentClassName}>
+        {label && (
+          <label className={styles.label} htmlFor={name}>
+            {label}
+          </label>
+        )}
+
+        <input
+          ref={this.textInput}
+          className={styles.input}
+          id={name}
+          maxLength="30"
+          name={name}
+          onChange={onChange}
+          placeholder={placeholder}
+          type="text"
+        />
+      </div>
+    );
+  }
 };
 
 export default TextInput;
+
+
+// import PropTypes from 'prop-types';
+// import React from 'react';
+// import classNames from 'classnames';
+
+// import styles from './TextInput.scss';
+
+// const TextInput = props => {
+//   const { className, label, name, onChange, placeholder } = props;
+
+// 	const componentClassName = classNames(styles.component, className);
+
+// 	return (
+//     <div className={componentClassName}>
+//       {label && (
+//         <label className={styles.label} htmlFor={name}>
+//           {label}
+//         </label>
+//       )}
+
+//       <input
+//         className={styles.input}
+//         id={name}
+//         maxLength="30"
+//         name={name}
+//         onChange={onChange}
+//         placeholder={placeholder}
+//         type="text"
+//       />
+//     </div>
+//   );
+// };
+
+// TextInput.propTypes = {
+// 	className: PropTypes.string,
+// 	label: PropTypes.string,
+// 	name: PropTypes.string,
+// 	onChange: PropTypes.func,
+// 	placeholder: PropTypes.string
+// };
+
+// TextInput.defaultProps = {
+// 	className: undefined,
+// 	label: undefined,
+// 	name: undefined,
+// 	onChange: undefined,
+// 	placeholder: undefined
+// };
+
+// export default TextInput;
